86f0c5182cf9dba991716c7eec580a9d7bc4d9ca
leacastagnettipro <lea.castagnetti.pro@gmail.com>
2026-03-04 16:49:33 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/JourFleuri/',
+  // On change '/JourFleuri/' par '/' car tu as un domaine personnalisé
+  base: '/', 
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
